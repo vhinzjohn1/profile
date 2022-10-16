2105942931190b58f1b8fd5e8d47c6be4680ddb8
vhinzjohn1 <77475601+vhinzjohn1@users.noreply.github.com>
2022-10-17 00:42:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1.5, stagger: 0.10 });
-tl.to(".slider", { y: "-100%", duration: 1, delay: 2 });
+tl.to(".slider", { y: "-100%", duration: 1, delay: 1 });
 tl.to(".intro", { y: "-100%", duration: 1.5}, "-=1" );
